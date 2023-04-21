cd2360a85c76c684cd412a63aca15d70afee53a6
kylsgl <kylsgl4@Gmail.com>
2023-04-21 16:04:41 +0800

    refactor: Remove unused deps and files

--- src/lib/sanity/index.ts
+++ src/lib/sanity/index.ts
@@ -3,4 +3,3 @@ export * from './api/getHero';
 export * from './api/getLinks';
 export * from './api/getProjects';
 export { default as sanityClient } from './client';
-export { default as urlFor } from './image';
